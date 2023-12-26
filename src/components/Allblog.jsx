8ed6153732de443a1e0a630de5436d56a6f1f022
Батболд Гансүх <batbold.gana@gmail.com>
2023-12-26 19:26:11 +0800

    zaa saihan boljiindoo

--- src/components/Allblog.jsx
+++ src/components/Allblog.jsx
@@ -12,73 +12,53 @@ export const Allblog = () => {
   }, []);
 
   return (
-    <div className="w-screen">
-      <div className="w-10/12 font-['Work_Sans'] text-2xl font-bold leading-7">
+    <div className="lg:w-screen flex justify-center items-center flex-col gap-12">
+      <div className="lg:w-10/12 font-['Work_Sans'] text-2xl font-bold leading-7">
         <h1>All Blog Post</h1>
       </div>
+      <div className=" flex flex-wrap gap-5 p-5 lg:w-10/12 lg:grid grid-cols-3 lg:gap-5">
+        {articles.map((article) => {
+          return (
+            <div className="  shadow-lg border rounded-xl p-4 flex  justify-between flex-col">
+              <img
+                className="rounded-xl h-auto w-full mb-4"
+                src={
+                  article.cover_image
+                    ? article.cover_image
+                    : "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg"
+                }
+                alt=""
+              />
+              <p className=" font-['Work_Sans'] text-sm py-1 px-2 bg-[#4B6BFB0D] text-[#4B6BFB] rounded-md w-fit">
+                tech
+              </p>
 
-      <div>
-        <div>
-          <img src={articles[0].cover_image} alt="" />
-          {articles[0].tag_list.map((tag) => (
-            <p>{tag}</p>
-          ))}
-          <h1>{articles[0].title}</h1>
-          <div>
-            <img src="" alt="" />
-            <h2></h2>
-            <div></div>
-          </div>
-        </div>
-        <div>
-          <img src={articles[0].cover_image} alt="" />
-          {articles[0].tag_list.map((tag) => (
-            <p>{tag}</p>
-          ))}
-          <h1>{articles[0].title}</h1>
-          <div>
-            <img src="" alt="" />
-            <h2></h2>
-            <div></div>
-          </div>
-        </div>
-        <div>
-          <img src={articles[0].cover_image} alt="" />
-          {articles[0].tag_list.map((tag) => (
-            <p>{tag}</p>
-          ))}
-          <h1>{articles[0].title}</h1>
-          <div>
-            <img src="" alt="" />
-            <h2></h2>
-            <div></div>
-          </div>
-        </div>
-        <div>
-          <img src={articles[0].cover_image} alt="" />
-          {articles[0].tag_list.map((tag) => (
-            <p>{tag}</p>
-          ))}
-          <h1>{articles[0].title}</h1>
-          <div>
-            <img src="" alt="" />
-            <h2></h2>
-            <div></div>
-          </div>
-        </div>
-        <div>
-          <img src={articles[0].cover_image} alt="" />
-          {articles[0].tag_list.map((tag) => (
-            <p>{tag}</p>
-          ))}
-          <h1>{articles[0].title}</h1>
-          <div>
-            <img src="" alt="" />
-            <h2></h2>
-            <div></div>
-          </div>
-        </div>
+              <h2 className="pt-4 pb-5 text-2xl font-semibold">
+                {article.title}
+              </h2>
+              <div className=" w-full flex  items-center">
+                <img
+                  className=" w-7 h-7   lg:w-9 lg:h-9 rounded-full "
+                  src={
+                    article.user.profile_image
+                      ? article.user.profile_image
+                      : "https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif"
+                  }
+                  alt=""
+                />
+                <p className="pl-3 pr-5 font-normal">{article.user.name}</p>
+                <p className="font-normal">
+                  {article.readable_publish_date} 2022
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </div>
+
+      <button className=" lg:w-10/12 font-['Work_Sans'] text-2xl p-5 border-solid leading-7">
+        Load More
+      </button>
     </div>
   );
 };
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Slider = () => {
   return (
-    <div className="w-screen h-1/4 flex justify-center">
+    <div className=" hidden lg:w-screen h-1/4 lg:flex justify-center">
       <div className=" w-10/12 h-full flex justify-center ">
         <img src="/Anai.png" alt="" />
       </div>
